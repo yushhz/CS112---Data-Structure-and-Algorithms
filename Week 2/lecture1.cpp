1e6c1520f15636ebcab1f494e4210d580b0ed98d
yushhz <ayush.kirpal7@gmail.com>
2026-07-27 14:36:26 +1200

    Lecture 1 Exercise

---
+++ Week 2/lecture1.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+
+  int n, sum;
+  sum = 0;
+  ifstream fin;
+  fin.open("numbers.txt");
+  // Check if the file is found and opened successfully
+
+  while (cin >> n)
+  {
+    cout << "Input from a user: " << n << endl;
+
+    if (n < 0)
+    {
+      cout << "Negative number, retry" << endl;
+      continue;
+    }
+    else if (n == 0)
+    {
+      cout << "Zero is not allowed, retry" << endl;
+      continue;
+    }
+    else
+    {
+      sum += n;
+    }
+  }
+  cout << "The sum of the numbers is: " << sum << endl;
+
+  while (fin >> n)
+  {
+    cout << "Input from a file: " << n << endl;
+  }
+
+  fin.close();
+  return 0;
+}
