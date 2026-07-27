66e8ece8a343dabb21755a07d4cfae9ecc67cd56
yushhz <ayush.kirpal7@gmail.com>
2026-07-27 14:42:04 +1200

    Printing sum into a txt file

--- Week 2/lecture1.cpp
+++ Week 2/lecture1.cpp
@@ -9,7 +9,9 @@ int main()
   int n, sum;
   sum = 0;
   ifstream fin;
+  ofstream fout;
   fin.open("numbers.txt");
+  fout.open("out.txt", ios::app);
   // Check if the file is found and opened successfully
 
   while (cin >> n)
@@ -36,8 +38,25 @@ int main()
   while (fin >> n)
   {
     cout << "Input from a file: " << n << endl;
+
+    if (n < 0)
+    {
+      cout << "Negative number, retry" << endl;
+      continue;
+    }
+    else if (n == 0)
+    {
+      cout << "Zero is not allowed, retry" << endl;
+      continue;
+    }
+    else
+    {
+      sum += n;
+    }
   }
+  fout << "The sum of the numbers is: " << sum << endl;
 
   fin.close();
+  fout.close();
   return 0;
 }
